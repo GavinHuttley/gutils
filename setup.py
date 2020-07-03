cb3e2f564b885e0c90bb219149275de96b780830
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2020-07-03 12:35:07 +1000

    ENH: initial commit
    [NEW] gutils invokes a CL interface for copying all data
        files from an assignment directory into a shared data folder
        and puts symlinks in to the original location. The goal is to
        eliminate redundant copying of large files by nbgrader.

---
+++ setup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import pathlib
+import sys
+
+from setuptools import find_packages, setup
+
+
+__author__ = "Gavin Huttley"
+__copyright__ = "Copyright 2014, Gavin Huttley"
+__credits__ = ["Gavin Huttley"]
+__license__ = "BSD-3"
+__version__ = "0.3"
+__maintainer__ = "Gavin Huttley"
+__email__ = "Gavin.Huttley@anu.edu.au"
+__status__ = "Development"
+
+# Check Python version, no point installing if unsupported version inplace
+if sys.version_info < (3, 6):
+    py_version = ".".join([str(n) for n in sys.version_info])
+    raise RuntimeError(
+        "Python-3.6 or greater is required, Python-%s used." % py_version
+    )
+
+short_description = "MutationMotif"
+
+readme_path = pathlib.Path(__file__).parent / "README.md"
+long_description = readme_path.read_text()
+PROJECT_URLS = {
+    "Documentation": "https://github.com/GavinHuttley/gutils",
+    "Bug Tracker": "https://github.com/GavinHuttley/gutils",
+    "Source Code": "https://github.com/GavinHuttley/gutils",
+}
+
+PACKAGE_DIR = "src"
+
+setup(
+    name="gutils",
+    version=__version__,
+    author="Gavin Huttley",
+    author_email="gavin.huttley@anu.edu.au",
+    description=short_description,
+    long_description=long_description,
+    long_description_content_type="text/md",
+    url="https://github.com/GavinHuttley/gutils",
+    platforms=["any"],
+    license=["BSD"],
+    keywords=[
+        "biology",
+        "genomics",
+        "genetics",
+        "statistics",
+        "evolution",
+        "bioinformatics",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "click",
+    ],
+    entry_points={
+        "console_scripts": [
+            "gutils=gutils.bundle_data:main",
+        ],
+    },
+    packages=find_packages(where="src"),
+    package_dir={"": PACKAGE_DIR},
+    project_urls=PROJECT_URLS,
+)
---
+++ src/gutils/bundle_data.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+"""bundles all non-python, non-r, non-ipynb script files into a dir
+replacing with symlinks"""
+import click
+import pathlib
+
+
+def get_data_paths(assign_dir: pathlib.Path, excludes):
+    moving = []
+    for fn in assign_dir.glob("**/*"):
+        if (
+            fn.suffix.lower() in excludes
+            or fn.name.startswith(".")
+            or fn.is_dir()
+            or fn.is_symlink()
+        ):
+            continue
+
+        moving.append(fn)
+    return moving
+
+
+def get_dest(assign_dir: pathlib.Path, dest_dir: pathlib.Path, path):
+    rel_to = path.relative_to(assign_dir)
+    dest = dest_dir / rel_to
+    dest_path = dest.relative_to(dest_dir)
+    dest_parent = dest_dir / dest_path.parent
+    return dest, dest_parent
+
+
+def valid_assignment_dir(assign_dir: pathlib.Path):
+    """an assignment dir must have at least one notebook"""
+    nbks = list(assign_dir.glob("*.ipynb"))
+    return len(nbks) >= 1
+
+
+@click.command()
+@click.argument("assign_dir", type=click.Path(exists=True))
+@click.argument("dest_root_dir", type=click.Path(), default="/home2/data")
+@click.option("-F", "--force", flag_value=True, help="force over write of dest_dir")
+@click.option("-D", "--dry_run", flag_value=True, help="display what will be done")
+def main(dest_root_dir, assign_dir, force, dry_run):
+    """copies data files (not directories) from assign_dir/ to dest_root_dir/assign_dir/data"""
+    cwd = pathlib.Path(".").absolute()
+    assign_dir = pathlib.Path(assign_dir).expanduser().absolute()
+    if not valid_assignment_dir(assign_dir):
+        click.secho(f"Assignment directories must contain a .ipynb file", fg="red")
+        exit()
+
+    assert assign_dir.is_dir(), "assign_dir must be a directory"
+
+    dest_root_dir = pathlib.Path(dest_root_dir).expanduser().absolute()
+    assert dest_root_dir.is_dir(), "dest_root_dir must be a directory"
+
+    dest_dir = dest_root_dir / assign_dir.name
+
+    excludes = [".py", ".r", ".ipynb", ".png", ".jpg", ".html"]
+    # get data paths
+    data_paths = get_data_paths(assign_dir, excludes)
+    created_paths = set()
+    for data_path in data_paths:
+        # create dest paths
+        dest, dest_parent = get_dest(assign_dir, dest_dir, data_path)
+        if dest.exists() and not force:
+            click.secho(
+                f"Exiting, '{dest}' already exists. Use -F to overwrite.", fg="red"
+            )
+            exit()
+
+        # create dirs for dest paths
+        if not dry_run:
+            dest_parent.mkdir(parents=True, exist_ok=True)
+            # move data to dest
+            dest = data_path.replace(dest)
+            # create symlink at original path
+            data_path.symlink_to(dest)
+        else:
+            if str(dest_parent) not in created_paths:
+                click.secho(f"Will create path: '{dest_parent}'", fg="blue")
+                created_paths.add(str(dest_parent))
+
+            click.secho(
+                f"Will move '{data_path.relative_to(cwd)}' to '{dest.relative_to(cwd)}'",
+                fg="green",
+            )
+            click.secho(
+                f"Will symlink '{dest.relative_to(cwd)}' to '{data_path.relative_to(cwd)}'",
+                fg="green",
+            )
+
+    click.secho("Done!", fg="green")
+
+
+if __name__ == "__main__":
+    main()
