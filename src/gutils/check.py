186daf0d0ad2f9f0fd3c8aee176f93840c8c3117
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2020-08-06 18:54:27 +1000

    improve formatting of error messages

--- src/gutils/check.py
+++ src/gutils/check.py
@@ -39,7 +39,8 @@ def expected_variables_values(names_values, scope):
 
         got = scope[name]
         if got != expect:
-            wrong.append((f"value of '{name}'='{got}' does not equal {expect}"))
+            expect = repr(expect) if isinstance(expect, str) else expect
+            wrong.append((f"value of {name}='{got}' does not equal {expect}"))
 
     if wrong:
         msg = "\n".join(wrong)
